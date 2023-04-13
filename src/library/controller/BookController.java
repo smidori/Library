a0c7526420b3b43d3ef2976413bd694a005a9e7a
MariaFerreiraCCT <2022443@student.cct.ie>
2023-04-13 11:56:07 +0100

    Bubble sort method added

--- src/library/controller/BookController.java
+++ src/library/controller/BookController.java
@@ -14,25 +14,58 @@ import library.utilities.ReadCSV;
  * @author Silvia Shimabuko
  */
 public class BookController {
-    
+
     public static final String FILE_BOOK = "BOOKS_DATA.csv";
-    
-    public List<Book> loadDataBooks(){
+
+    public List<Book> loadDataBooks() {
         List<Book> books = new ArrayList<>();
         ReadCSV reader = new ReadCSV();
 
         List<String> lines = reader.readFile(FILE_BOOK);
-        
-        if(lines != null){
-            for(String line: lines){
+
+        if (lines != null) {
+            for (String line : lines) {
                 //this pattern is to ignore to slit by comma when it is inside of quotes
                 String[] data = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-                
-                Book book = new Book(data[0],data[1],data[2], data[3],data[4]);
+
+                Book book = new Book(data[0], data[1], data[2], data[3], data[4]);
                 books.add(book);
             }
         }
-        
+
         return books;
     }
+
+    public List<Book> bubbleSorted(List<Book> array, String orderBy) {
+        Book temp;
+        boolean swap = false;
+        do {
+            swap = false;
+            String s1;
+            String s2;
+
+            for (int j = 0; j < array.size() - 1; j++) {
+
+                if (orderBy.equalsIgnoreCase("author")) {
+                    s1 = array.get(j).getFullName();
+                    s2 = array.get(j + 1).getFullName();
+                } else {//order by title
+                    s1 = array.get(j).getTitle();
+                    s2 = array.get(j + 1).getTitle();
+                }
+
+                int compare = s1.compareTo(s2);
+
+                if (compare > 0) { //s1 > s2
+                    temp = array.get(j);
+                    array.set(j, array.get(j + 1));
+                    array.set(j + 1, temp);
+                    swap = true;
+                }
+            }
+        } while (swap);
+        System.out.println("-----------");
+        return array;
+    }
+
 }
